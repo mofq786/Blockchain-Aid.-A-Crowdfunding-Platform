323ab20518e810690d1877b528c19b9f4dc66be7
MD Omar Farooq Qureshi <134828473+mofq786@users.noreply.github.com>
2024-08-21 10:05:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
   build: {
     target: 'es2021', // or 'esnext'
     base: '/',
